001c0dd28599a5905a32bea7008b92a5af2fdda7
Isaac Ramirez <isaac.ramirez.herrera@gmail.com>
2020-05-20 20:16:13 -0600

    Update Main.java

--- src/conflictos/Main.java
+++ src/conflictos/Main.java
@@ -2,6 +2,6 @@ package conflictos;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hola!!!!!!Mundo");		
+		System.out.println("Hola!!!!!!Mundo!!!!!!!!!!");		
 	}
 }
